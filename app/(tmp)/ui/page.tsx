0cd5b4057ecf9590a4729f73e97048774a0239ee
kcwww <cwkim0321@gmail.com>
2024-04-04 02:44:26 +0900

    feat : add UI page

---
+++ app/(tmp)/ui/page.tsx
@@ -0,0 +1,21 @@
+import AccordionSection from '@/app/(tmp)/_components/sections/AccordionSection';
+import ButtonSection from '@/app/(tmp)/_components/sections/ButtonSection';
+import CardSection from '@/app/(tmp)/_components/sections/CardSection';
+import InputSection from '@/app/(tmp)/_components/sections/InputSection';
+import LabelSection from '@/app/(tmp)/_components/sections/LabelSection';
+import RadioGroupSection from '@/app/(tmp)/_components/sections/RadioGroupSection';
+
+const UIPage = () => {
+  return (
+    <main>
+      <AccordionSection />
+      <ButtonSection />
+      <CardSection />
+      <InputSection />
+      <LabelSection />
+      <RadioGroupSection />
+    </main>
+  );
+};
+
+export default UIPage;
