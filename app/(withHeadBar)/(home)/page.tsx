05ac11007820c70b1dbbf8ee591181ba51e00077
unknown <5nxtnxtnxt@gmail.com>
2024-05-03 20:22:19 +0900

    layout: navigation, HeadBar layout
    navigation routing 됨
    HeadBar 기능x

--- app/(withHeadBar)/(home)/page.tsx
+++ app/(withHeadBar)/(home)/page.tsx
@@ -2,7 +2,7 @@ import Image from 'next/image';
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex flex-col items-center justify-between">
       <Image
         src="/vercel.svg"
         alt="Vercel Logo"
@@ -11,6 +11,7 @@ export default function Home() {
         height={24}
         priority
       />
+      <h1>home page</h1>
     </main>
   );
 }
---
+++ components/headBar.tsx
@@ -0,0 +1,19 @@
+'use client';
+
+import { usePathname } from 'next/navigation';
+import { Button } from './ui/button';
+
+export default function HeadBar() {
+  const path = usePathname().split('/')[1];
+  const title = path === '' ? 'home' : path;
+  return (
+    <div className="flex justify-between w-full h-16 items-center p-4 bg-primary">
+      <Button className="hover:bg-slate-500">{title}</Button>
+      <div>
+        <Button className="hover:bg-slate-500">chat</Button>
+        <Button className="hover:bg-slate-500">search</Button>
+        <Button className="hover:bg-slate-500">notice</Button>
+      </div>
+    </div>
+  );
+}
