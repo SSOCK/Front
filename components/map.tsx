efffe9b7cdf3052179384752eeb426997571a0df
esthel <esthel9026@naver.com>
2024-05-09 00:26:39 +0900

    fix: window.kakao.maps 변수화 -> KakaoMaps

--- components/map.tsx
+++ components/map.tsx
@@ -14,8 +14,8 @@ interface ClickLatLng {
 }
 
 export default function Map() {
-  const [lat, setLat] = useState<number>(33.450701);
-  const [lng, setLng] = useState<number>(126.570667);
+  const [lat, setLat] = useState<number>(0);
+  const [lng, setLng] = useState<number>(0);
 
   useEffect(() => {
     if (navigator.geolocation) {
@@ -28,23 +28,25 @@ export default function Map() {
   }, []);
 
   useEffect(() => {
-    window.kakao.maps.load(() => {
+    const KakaoMaps = window.kakao.maps;
+
+    KakaoMaps.load(() => {
       const container = document.getElementById('map');
       const options = {
-        center: new window.kakao.maps.LatLng(lat, lng),
+        center: new KakaoMaps.LatLng(lat, lng),
         level: 3,
       };
 
-      const map = new window.kakao.maps.Map(container, options);
+      const map = new KakaoMaps.Map(container, options);
       const mapDom = map.a as Document; // 이벤트 다루기위함
 
       const linePath1 = [
-        new window.kakao.maps.LatLng(33.452344169439975, 126.56878163224233),
-        new window.kakao.maps.LatLng(33.452739313807456, 126.5709308145358),
-        new window.kakao.maps.LatLng(33.45178067090639, 126.5726886938753),
+        new KakaoMaps.LatLng(33.452344169439975, 126.56878163224233),
+        new KakaoMaps.LatLng(33.452739313807456, 126.5709308145358),
+        new KakaoMaps.LatLng(33.45178067090639, 126.5726886938753),
       ];
 
-      const line = new window.kakao.maps.Polyline({
+      const line = new KakaoMaps.Polyline({
         path: linePath1,
         strokeWeight: 10,
         strokeColor: '#FFAE00',
@@ -70,7 +72,7 @@ export default function Map() {
       });
 
       // click (해당 위도, 경도 표출)
-      window.kakao.maps.event.addListener(
+      KakaoMaps.event.addListener(
         map,
         'click',
         (mouseEvent: { latLng: ClickLatLng }) => {
@@ -80,7 +82,7 @@ export default function Map() {
       );
 
       // bounds_changed (지도 확대축소, 중심 이동 등)
-      window.kakao.maps.event.addListener(map, 'bounds_changed', () => {
+      KakaoMaps.event.addListener(map, 'bounds_changed', () => {
         const mapImg = mapDom.querySelectorAll('img');
         mapImg.forEach((img) => {
           img.classList.add('grayscale');
