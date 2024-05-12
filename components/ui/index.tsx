19180c7864dd06264b160d4249bca726dc3aa133
esthel <esthel9026@naver.com>
2024-05-12 19:16:26 +0900

    refactor: shadcn index추가

--- components/ui/index.tsx
+++ components/ui/index.tsx
@@ -1,2 +1,15 @@
 export { Avatar, AvatarFallback, AvatarImage } from './avatar';
 export { Button, buttonVariants } from './button';
+export { Checkbox } from './checkbox';
+export {
+  useFormField,
+  Form,
+  FormItem,
+  FormLabel,
+  FormControl,
+  FormDescription,
+  FormMessage,
+  FormField,
+} from './form';
+export { Input } from './input';
+export { Label } from './label';
