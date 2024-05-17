c9524dcf16aa319b0552b787a23e05d00342f990
5nxtnxtnxt <5nxtnxtnxt@gmail.com>
2024-05-17 14:39:50 +0900

    test

--- app/map/page.tsx
+++ app/map/page.tsx
@@ -1,11 +1,22 @@
+'use client';
+
+import { useRef } from 'react';
 import { HeadBar, Map, Navigation } from '@components';
+import { Button } from '@shadcn';
 
 export default function Home() {
+  const mode = useRef(false);
+  const c = () => {
+    mode.current = !mode.current;
+  };
   return (
     <>
       <HeadBar />
       <main className="mainPart w-full h-full">
         <Map />
+        <Button className="fixed z-50" onClick={c}>
+          test
+        </Button>
       </main>
       <Navigation />
     </>
