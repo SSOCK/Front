0a77efbbad9804a95f3e756d5f842e216ba6584d
5nxtnxtnxt <5nxtnxtnxt@gmail.com>
2024-06-05 15:37:51 +0900

    feat: 401 Unautorized 처리
    utils의 fetchWithRetry메소드 생성후 사용

--- utils/fetch.tsx
+++ utils/fetch.tsx
@@ -1,8 +1,39 @@
-export async function tryAuthWithRefreshToken() {
+export async function refreshAccessToken() {
   const respones = await fetch('/api/auth/refresh', {
     method: 'post',
     credentials: 'include',
   });
-  const data = await respones.json();
-  console.log(data);
+  if (respones.status !== 201) {
+    //실패시 처리
+    return;
+  }
+  if (!respones.ok) throw new Error('accessToken 재발급에 실패했습니다.');
+
+  const data: {
+    'access-token': string;
+  } = await respones.json();
+
+  sessionStorage.setItem('access-token', `Bearer ${data['access-token']}`);
+  return;
+}
+export async function fetchWithRetry(url: string, options: RequestInit) {
+  try {
+    options.headers = { ...options.headers, Authorization: getAccessToken() };
+    const respones = await fetch(url, options);
+    if (respones.ok) {
+      const data = await respones.json();
+      return data;
+    }
+    await refreshAccessToken();
+    options.headers = { ...options.headers, Authorization: getAccessToken() };
+    const respones2 = await fetch(url, options);
+    const data = await respones2.json();
+    return data;
+  } catch (error) {
+    console.error('fetch중 에러발생:', error);
+  }
+}
+
+export function getAccessToken() {
+  return sessionStorage.getItem('access-token') ?? 'no-token';
 }
