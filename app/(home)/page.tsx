6f18452fe2a8126fe3ca82719af5504033e60bc2
5nxtnxtnxt <5nxtnxtnxt@gmail.com>
2024-06-05 20:59:22 +0900

    feat: home접속시 자동로그인 시도
    자동로그인 시도 후 로그인 실패하면 logout()

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -3,15 +3,22 @@
 import { useEffect } from 'react';
 import { HeadBar, Navigation, Post } from '@components';
 import useLogout from '@hooks/useLogout';
+import { refreshAccessToken } from '@/utils/fetch';
 import Posts from './posts';
 
 export default function Home() {
   const logout = useLogout();
 
   useEffect(() => {
-    if (!sessionStorage.getItem('access-token')) {
-      //logout();
-    }
+    const aa = async () => {
+      await refreshAccessToken();
+      console.log(sessionStorage.getItem('access-token'));
+
+      if (!sessionStorage.getItem('access-token')) {
+        logout();
+      }
+    };
+    aa();
 
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
