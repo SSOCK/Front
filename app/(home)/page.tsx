a5485699c692bae91728304837d45ae50eab196a
esthel <esthel9026@naver.com>
2024-06-11 01:08:38 +0900

    style: 코드정리

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,25 +1,17 @@
 'use client';
 
 import { useEffect } from 'react';
-import { HeadBar, Navigation, Post } from '@components';
+import { HeadBar, Navigation } from '@components';
 import useLogout from '@hooks/useLogout';
-import { refreshAccessToken } from '@/utils/fetch';
 import Posts from './posts';
 
 export default function Home() {
   const logout = useLogout();
 
   useEffect(() => {
-    const aa = async () => {
-      await refreshAccessToken();
-      console.log(sessionStorage.getItem('access-token'));
-
-      if (!sessionStorage.getItem('access-token')) {
-        logout();
-      }
-    };
-    aa();
-
+    if (!sessionStorage.getItem('access-token')) {
+      logout();
+    }
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
@@ -27,7 +19,7 @@ export default function Home() {
     <>
       <HeadBar />
       <main className="mainPart flex flex-col items-center justify-between">
-        <Posts></Posts>
+        <Posts />
       </main>
       <Navigation />
     </>
