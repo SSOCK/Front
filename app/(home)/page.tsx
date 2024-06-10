30a9a7828b60323df97a3409e7ff4c1ee3ffa082
esthel <esthel9026@naver.com>
2024-06-11 02:37:14 +0900

    fix: merge conflict 복구

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -3,15 +3,20 @@
 import { useEffect } from 'react';
 import { HeadBar, Navigation } from '@components';
 import useLogout from '@hooks/useLogout';
+import { refreshAccessToken } from '@/utils/fetch';
 import Posts from './posts';
 
 export default function Home() {
   const logout = useLogout();
 
   useEffect(() => {
-    if (!sessionStorage.getItem('access-token')) {
-      logout();
-    }
+    const checkToken = async () => {
+      await refreshAccessToken();
+      if (!sessionStorage.getItem('access-token')) {
+        logout();
+      }
+    };
+    checkToken();
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
