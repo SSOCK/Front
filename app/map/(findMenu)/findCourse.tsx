272ee895f680b3f34a29fbedd9f8d14cbbecd537
5nxtnxtnxt <5nxtnxtnxt@gmail.com>
2024-06-19 17:44:24 +0900

    feat: 주소정보 받아서 표시

--- app/map/(findMenu)/findCourse.tsx
+++ app/map/(findMenu)/findCourse.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import { useState } from 'react';
 import { MyMap } from '@/app/map/page';
 import AroundCourse from './aroundCourse';
 import SearchInput from './searchInput';
@@ -9,14 +10,15 @@ interface FindCourseProps {
 }
 
 export default function FindCourse({ mapRef }: FindCourseProps) {
+  const [nowPlace, setNowPlace] = useState('제주시 아라1동');
   return (
     <div className="flex flex-col h-full">
       <div className="flex flex-col p-3 gap-6">
         <SearchInput />
-        <h3 className=" font-bold text-lg">청주시 방구동 </h3>
+        <h3 className="font-bold text-lg">{nowPlace}</h3>
       </div>
       <div className="grow overflow-y-scroll scroll">
-        <AroundCourse mapRef={mapRef} />
+        <AroundCourse mapRef={mapRef} setNowPlace={setNowPlace} />
       </div>
     </div>
   );
