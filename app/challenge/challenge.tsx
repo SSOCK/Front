1f26d8298d770f6c2f351ec9554024161d7d7deb
esthel <esthel9026@naver.com>
2024-06-20 18:13:49 +0900

    design: challenge

--- app/challenge/challenge.tsx
+++ app/challenge/challenge.tsx
@@ -14,15 +14,15 @@ export default function Challenge({
   participate,
 }: ChallengeProps) {
   return (
-    <div className="bg-red-300 flex flex-col p-3 cursor-pointer">
-      <div className="overflow-hidden basis-2/3">
-        <img className="rounded-md" src={img} alt="challengeImg" />
+    <div className="flex flex-col p-3 cursor-pointer h-72">
+      <div className="overflow-hidden basis-2/3 rounded-md border-2">
+        <img className="" src={img} alt="challengeImg" />
       </div>
       <div className="pb-5 basis-1/3 pt-2">
-        <div className="font-bold text-lg">{title}</div>
-        <div>{introduce}</div>
-        <div>{period}</div>
-        <div>{participate}명 참여중</div>
+        <div className="font-bold text-md">{title}</div>
+        <div className="text-sm">{introduce}</div>
+        <div className="text-sm">{period}</div>
+        <div className="text-sm">{participate}명 참여중</div>
       </div>
     </div>
   );
