86ce3a8c171f308b85da61c21c30933f616cdaef
esthel <esthel9026@naver.com>
2024-06-20 20:51:00 +0900

    design: 댓글창 css

--- components/post.tsx
+++ components/post.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-// import { useState } from 'react';
+import { useState, useRef } from 'react';
 import {
   Carousel,
   CarouselContent,
@@ -13,13 +13,25 @@ import Comment from '@/public/icons/comment.svg';
 import Like from '@/public/icons/like.svg';
 import { Avatar, AvatarFallback } from './ui/avatar';
 import { Button } from './ui/button';
+import { Input } from './ui/input';
 
 type PostProps = {
   postData: PostType;
 };
 
 export default function Post({ postData }: PostProps) {
-  // const [viewComment, setViewComment] = useState(false);
+  const [viewComment, setViewComment] = useState(false);
+  const [comment, setComment] = useState([]);
+  const [commentError, setCommentError] = useState(false);
+  const commentRef = useRef<HTMLInputElement>(null);
+
+  const sendComment = () => {
+    if (!commentRef.current?.value) {
+      setCommentError(true);
+      return;
+    }
+    alert(commentRef.current?.value);
+  };
 
   return (
     <div className="w-full p-5">
@@ -78,7 +90,7 @@ export default function Post({ postData }: PostProps) {
           <Button
             variant="ghost"
             className="p-0"
-            // onClick={() => setViewComment(!viewComment)}
+            onClick={() => setViewComment(!viewComment)}
           >
             <Comment className="mr-2" />
             <h5>댓글 {postData.comments.length}개</h5>
@@ -86,11 +98,54 @@ export default function Post({ postData }: PostProps) {
         </div>
       </div>
 
-      {/* {viewComment ? (
-        <div className="bg-black">
-          <Button onClick={() => setViewComment(false)}>x</Button>
+      {viewComment ? (
+        <div className="bg-red-400">
+          <Button
+            className="pl-auto mb-2"
+            onClick={() => setViewComment(false)}
+          >
+            x
+          </Button>
+          <div className="flex pb-5">
+            <Input
+              ref={commentRef}
+              placeholder="댓글을 입력해주세요"
+              className="mr-2"
+              onChange={() => setCommentError(false)}
+            />
+            <Button onClick={sendComment}>전송</Button>
+          </div>
+          {commentError ? (
+            <p className="text-right text-red-500">
+              댓글은 한글자 이상 입력 가능합니다.
+            </p>
+          ) : null}
+
+          {comment.length ? (
+            <div className="p-5">
+              <div className="flex flex-row gap-4 flex-auto items-center cursor-pointer">
+                <Avatar className=" size-12">
+                  <AvatarFallback>
+                    <h6 className="text-xs">댓글작성자이름</h6>
+                  </AvatarFallback>
+                </Avatar>
+
+                <div className="flex flex-col gap-1">
+                  <h1 className="text-xs font-semibold">댓글작성자이름</h1>
+                  <h2 className="text-xs">
+                    {/* {getPostTime(postData.modifiedAt ?? postData.createdAt)} */}
+                    몇 분 전
+                  </h2>
+                </div>
+              </div>
+
+              <div className="pt-2 ml-16">
+                댓글내용블라블라댓글내용블라블라댓글내용블라블라댓글내용블라블라댓글내용블라블라댓글내용블라블라댓글내용블라블라댓글내용블라블라댓글내용블라블라댓글내용블라블라댓글내용블라블라댓글내용블라블라댓글내용블라블라댓글내용블라블라댓글내용블라블라댓글내용블라블라댓글내용블라블라댓글내용블라블라
+              </div>
+            </div>
+          ) : null}
         </div>
-      ) : null} */}
+      ) : null}
     </div>
   );
 }
