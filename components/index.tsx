e737f09719234cffa805f1cb17233b0099e67b11
esthel <esthel9026@naver.com>
2024-06-23 20:42:33 +0900

    rename: club, challenge to component

--- components/index.tsx
+++ components/index.tsx
@@ -2,3 +2,5 @@ export { default as HeadBar } from './headBar';
 export { default as Map } from './map';
 export { default as Navigation } from './navigation';
 export { default as Post } from './post';
+export { default as Challenge } from './challenge';
+export { default as Club } from './club';
