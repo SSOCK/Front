5c2993965b4f21da4226de2f618d91b4f6a1ee05
esthel <esthel9026@naver.com>
2024-06-23 22:16:44 +0900

    fix: club css

--- components/club.tsx
+++ components/club.tsx
@@ -7,11 +7,11 @@ interface ClubProps {
 
 export default function Club({ title, img, loc, participate }: ClubProps) {
   return (
-    <div className="flex flex-col p-3 cursor-pointer h-80">
-      <div className="overflow-hidden basis-2/3 rounded-md border-2">
+    <div className="flex flex-col p-3 cursor-pointer h-64">
+      <div className="overflow-hidden basis-3/4 rounded-md border-2">
         <img className="" src={img} alt="challengeImg" />
       </div>
-      <div className="pb-5 basis-1/3 pt-2">
+      <div className="basis-1/4 pt-2">
         <div className="font-bold text-md">{title}</div>
         <div className="flex">
           <div className="text-sm pr-2">📍{loc}</div>
