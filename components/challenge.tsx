a754eda288b1b61ae32fd933fc26e9f565f1886e
esthel <esthel9026@naver.com>
2024-06-23 22:29:05 +0900

    fix: challenge component

--- components/challenge.tsx
+++ components/challenge.tsx
@@ -14,11 +14,11 @@ export default function Challenge({
   participate,
 }: ChallengeProps) {
   return (
-    <div className="flex flex-col p-3 cursor-pointer h-72">
+    <div className="flex flex-col p-3 cursor-pointer h-64">
       <div className="overflow-hidden basis-2/3 rounded-md border-2">
         <img className="" src={img} alt="challengeImg" />
       </div>
-      <div className="pb-5 basis-1/3 pt-2">
+      <div className="basis-1/3 pt-2">
         <div className="font-bold text-md">{title}</div>
         <div className="text-sm">{introduce}</div>
         <div className="text-sm">{period}</div>
