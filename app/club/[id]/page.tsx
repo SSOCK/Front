ab9c7d9787e0aede9903d03940ca02dbe4f70f7d
esthel <esthel9026@naver.com>
2024-07-07 18:30:58 +0900

    feat: club member

--- app/club/[id]/page.tsx
+++ app/club/[id]/page.tsx
@@ -3,6 +3,7 @@
 import { useState } from 'react';
 import { HeadBar } from '@components';
 import { Button } from '@components/ui/button';
+import Member from './member';
 import Rank from './rank';
 
 export default function Home() {
@@ -92,7 +93,7 @@ export default function Home() {
 
             <div className="pt-10">
               {list.순위 ? <Rank club={name} /> : null}
-              {list.멤버 ? <></> : null}
+              {list.멤버 ? <Member club={name} /> : null}
               {list.일정 ? <></> : null}
               {list.게시판 ? <></> : null}
               {list.채팅 ? <></> : null}
