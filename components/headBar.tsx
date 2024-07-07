ff36804ce0f940f74d6bd1d32dcefb38b097edcd
esthel <esthel9026@naver.com>
2024-07-07 21:48:12 +0900

    fix: headbar 내용 수정 및 반응형 구현

--- components/headBar.tsx
+++ components/headBar.tsx
@@ -6,17 +6,19 @@ import { cn } from '@/lib/utils';
 import { Button } from './ui/button';
 
 const paths = [
-  { path: '', category: '피드' },
+  { path: '', category: '홈' },
   { path: 'map', category: '코스' },
-  { path: 'club', category: '클럽/챌린지' },
+  { path: 'club', category: '클럽' },
+  { path: 'challenge', category: '챌린지' },
+  { path: 'feed', category: '피드' },
 ];
 
 export default function HeadBar() {
   const nowPath = usePathname().split('/')[1];
   return (
-    <div className="flex justify-between w-full h-16 items-center p-4 z-50 border-b">
+    <div className="flex justify-between w-full h-16 items-center p-4 z-50 border-b xl:w-5/6 xl:mx-auto">
       <div>
-        <Button variant="link" className="text-xl font-bold ">
+        <Button variant="link" className="text-xl font-bold hidden sm:inline">
           RunningMate
         </Button>
         {paths.map(({ path, category }) => (
