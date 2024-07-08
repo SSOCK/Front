e3418b6603cdca9327c2cf976c149a160ac3029f
esthel <esthel9026@naver.com>
2024-07-08 18:55:26 +0900

    design: 1차 headbar
    popup 구현 안함

--- components/headBar.tsx
+++ components/headBar.tsx
@@ -3,6 +3,8 @@
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 import { cn } from '@/lib/utils';
+import Add from '@/public/icons/add.svg';
+import Bell from '@/public/icons/bell.svg';
 import { Button } from './ui/button';
 
 const paths = [
@@ -15,8 +17,10 @@ const paths = [
 
 export default function HeadBar() {
   const nowPath = usePathname().split('/')[1];
+  const img = 'https://avatars.githubusercontent.com/u/96722691?v=5';
+
   return (
-    <div className="flex justify-between w-full h-16 items-center p-4 z-50 border-b xl:w-5/6 xl:mx-auto">
+    <div className="flex justify-between w-full h-16 items-center p-2 z-50 border-b xl:w-5/6 xl:mx-auto xl:p-4">
       <div>
         <Button variant="link" className="text-xl font-bold hidden sm:inline">
           RunningMate
@@ -25,14 +29,25 @@ export default function HeadBar() {
           <Button
             key={category}
             variant="link"
-            className={cn(['font-bold', nowPath !== path && 'text-black'])}
+            className={cn([
+              'font-bold p-2 sm:p-4',
+              nowPath !== path && 'text-black',
+            ])}
           >
             <Link href={`/${path}`}>{category}</Link>
           </Button>
         ))}
       </div>
 
-      <div></div>
+      <div className="flex gap-3 sm:gap-5 sm:pr-4">
+        <Add className="w-6 fill-primary cursor-pointer" />
+        <Bell className="w-5 cursor-pointer" />
+        <img
+          className="w-8 h-8 rounded-full border bg-slate-400 cursor-pointer"
+          src={img}
+          alt=""
+        />
+      </div>
     </div>
   );
 }
