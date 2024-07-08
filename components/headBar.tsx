b72cc2468985c810254e11cbf94cf9d673ff97cc
esthel <esthel9026@naver.com>
2024-07-08 19:55:45 +0900

    design: headbar popup 구현

--- components/headBar.tsx
+++ components/headBar.tsx
@@ -2,6 +2,7 @@
 
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
+import { ReactNode, useEffect, useRef, useState } from 'react';
 import { cn } from '@/lib/utils';
 import Add from '@/public/icons/add.svg';
 import Bell from '@/public/icons/bell.svg';
@@ -16,8 +17,34 @@ const paths = [
 ];
 
 export default function HeadBar() {
+  const [add, setAdd] = useState(false);
+  const [bell, setBell] = useState(false);
+  const [alarm, setAlarm] = useState<Array<ReactNode>>([]);
+  const addRef = useRef<HTMLDivElement>(null);
+  const bellRef = useRef<HTMLDivElement>(null);
+
   const nowPath = usePathname().split('/')[1];
   const img = 'https://avatars.githubusercontent.com/u/96722691?v=5';
+  const elemClass = 'p-2 bg-white cursor-pointer hover:bg-border';
+  const alarmData = [
+    '알람 1입니다.',
+    '알람 2입니다.',
+    '알람 3입니다.',
+    '알람 4입니다.',
+  ];
+
+  useEffect(() => {
+    setAlarm(
+      alarmData.map((item, index) => (
+        <div key={index} className={elemClass}>
+          {item}
+        </div>
+      ))
+    );
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  const moveToMypage = () => (window.location.href = '/mypage');
 
   return (
     <div className="flex justify-between w-full h-16 items-center p-2 z-50 border-b xl:w-5/6 xl:mx-auto xl:p-4">
@@ -40,12 +67,49 @@ export default function HeadBar() {
       </div>
 
       <div className="flex gap-3 sm:gap-5 sm:pr-4">
-        <Add className="w-6 fill-primary cursor-pointer" />
-        <Bell className="w-5 cursor-pointer" />
+        <div className="relative w-6 flex items-center">
+          <Add
+            className="w-full fill-primary cursor-pointer relative"
+            onClick={() => {
+              setBell(false);
+              bellRef.current!.style.display = 'none';
+              setAdd(!add);
+              addRef.current!.style.display = add ? 'none' : 'block';
+            }}
+          />
+          <div
+            ref={addRef}
+            className="hidden w-40 absolute top-9 right-0 border"
+          >
+            <div className={elemClass}>활동 기록</div>
+            <div className={elemClass}>코스 등록</div>
+            <div className={elemClass}>게시글 작성</div>
+          </div>
+        </div>
+
+        <div className="relative w-5 flex items-center">
+          <Bell
+            className="w-full cursor-pointer hover:fill-primary"
+            onClick={() => {
+              setAdd(false);
+              addRef.current!.style.display = 'none';
+              setBell(!bell);
+              bellRef.current!.style.display = bell ? 'none' : 'block';
+            }}
+          />
+          <div
+            ref={bellRef}
+            className="hidden w-56 absolute top-9 right-0 border"
+          >
+            {alarm}
+          </div>
+        </div>
+
         <img
           className="w-8 h-8 rounded-full border bg-slate-400 cursor-pointer"
           src={img}
           alt=""
+          onClick={moveToMypage}
         />
       </div>
     </div>
