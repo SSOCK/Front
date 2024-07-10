ca6432cfc351ee2eca3a7f6bf9d754214d812bd4
esthel <esthel9026@naver.com>
2024-07-10 19:11:42 +0900

    design: mypage 웹 메뉴바

--- app/mypage/page.tsx
+++ app/mypage/page.tsx
@@ -20,6 +20,7 @@ export default function Home() {
   const settingRef = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
+    reset();
     setProfile(true);
   }, []);
 
@@ -33,9 +34,69 @@ export default function Home() {
     setSetting(false);
   };
 
+  const Menu = ({
+    base,
+    click,
+    border,
+  }: {
+    base: string;
+    click: string;
+    border: boolean;
+  }) => {
+    return (
+      <>
+        <div
+          ref={profileRef}
+          id="item"
+          className={base + (profile ? click : '')}
+        >
+          프로필
+        </div>
+        {border ? <div className="border" /> : null}
+        <div
+          ref={activityRef}
+          id="item"
+          className={base + (activity ? click : '')}
+        >
+          활동
+        </div>
+        {border ? <div className="border" /> : null}
+        <div ref={courseRef} id="item" className={base + (course ? click : '')}>
+          코스
+        </div>
+        {border ? <div className="border" /> : null}
+        <div ref={clubRef} id="item" className={base + (club ? click : '')}>
+          클럽
+        </div>
+        {border ? <div className="border" /> : null}
+        <div
+          ref={challengeRef}
+          id="item"
+          className={base + (challenge ? click : '')}
+        >
+          챌린지
+        </div>
+        {border ? <div className="border" /> : null}
+        <div ref={feedRef} id="item" className={base + (feed ? click : '')}>
+          피드
+        </div>
+        {border ? <div className="border" /> : null}
+        <div
+          ref={settingRef}
+          id="item"
+          className={base + (setting ? click : '')}
+        >
+          설정
+        </div>
+      </>
+    );
+  };
+
   const change = (event: MouseEvent<HTMLDivElement>) => {
-    reset();
     const target = event.target as HTMLElement;
+    if (target.id !== 'item') return;
+
+    reset();
     switch (target.innerText) {
       case '프로필':
         setProfile(true);
@@ -65,79 +126,25 @@ export default function Home() {
     <>
       <HeadBar />
       <div className="w-full sm:flex xl:w-5/6 xl:mx-auto">
-        <div className="hidden border sm:basis-1/6 sm:block">마이페이지</div>
+        <div
+          className="hidden py-5 px-3 sm:basis-1/6 sm:block"
+          onClick={change}
+        >
+          <div className="font-bold pb-2">마이페이지</div>
+          <Menu
+            base="cursor-pointer border p-2 hover:bg-primary hover:text-white"
+            click=" bg-primary border-primary text-white"
+            border={false}
+          />
+        </div>
         <div className="p-2 sm:hidden">
           <div className="font-bold pb-2">마이페이지</div>
           <div className="flex justify-between" onClick={change}>
-            <div
-              ref={profileRef}
-              className={
-                'cursor-pointer hover:text-primary' +
-                (profile ? ' text-primary' : '')
-              }
-            >
-              프로필
-            </div>
-            <div className="border" />
-            <div
-              ref={activityRef}
-              className={
-                'cursor-pointer hover:text-primary' +
-                (activity ? ' text-primary' : '')
-              }
-            >
-              활동
-            </div>
-            <div className="border" />
-            <div
-              ref={courseRef}
-              className={
-                'cursor-pointer hover:text-primary' +
-                (course ? ' text-primary' : '')
-              }
-            >
-              코스
-            </div>
-            <div className="border" />
-            <div
-              ref={clubRef}
-              className={
-                'cursor-pointer hover:text-primary' +
-                (club ? ' text-primary' : '')
-              }
-            >
-              클럽
-            </div>
-            <div className="border" />
-            <div
-              ref={challengeRef}
-              className={
-                'cursor-pointer hover:text-primary' +
-                (challenge ? ' text-primary' : '')
-              }
-            >
-              챌린지
-            </div>
-            <div className="border" />
-            <div
-              ref={feedRef}
-              className={
-                'cursor-pointer hover:text-primary' +
-                (feed ? ' text-primary' : '')
-              }
-            >
-              피드
-            </div>
-            <div className="border" />
-            <div
-              ref={settingRef}
-              className={
-                'cursor-pointer hover:text-primary' +
-                (setting ? ' text-primary' : '')
-              }
-            >
-              설정
-            </div>
+            <Menu
+              base="cursor-pointer hover:text-primary"
+              click=" text-primary"
+              border={true}
+            />
           </div>
         </div>
 
