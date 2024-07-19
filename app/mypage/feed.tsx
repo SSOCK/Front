4a8050e6e3e201a405cb8a9ad2c36084341520db
esthel <esthel9026@naver.com>
2024-07-19 20:39:51 +0900

    feat: mypage feed api 연결

--- app/mypage/feed.tsx
+++ app/mypage/feed.tsx
@@ -1,46 +1,45 @@
+import { useEffect, useState, useRef } from 'react';
 import { Post } from '@components';
+import { loadPost } from '@utils/post';
 
 export default function Feed() {
-  const data = {
-    follow: 100,
-    following: 200,
-    postCount: 1,
-    posts: [
-      {
-        content: '내용을 작성해주세요. 블라블라어쩌구~',
-        createdAt: '2024-07-02T00:57:09.528969',
-        id: 123,
-        imageUrls: ['https://avatars.githubusercontent.com/u/96722691?v=5'],
-        modifiedAt: null,
-        title: '제목',
-        member: {
-          email: 'unknown@unknown.com',
-          id: 123,
-          name: '이름',
-          username: '유저네임',
-          profilePicture:
-            'https://avatars.githubusercontent.com/u/96722691?v=5',
-        },
-        likes: 900,
-        comments: [
-          {
-            id: 456,
-            content: '댓글단다ㅋ',
-            username: 'test',
-            createdAt: '2024-07-04T00:57:09.528969',
-            postId: 132,
-          },
-        ],
+  const [data, setData] = useState<PostType[]>([]);
+  const [lastPostId, setLastPostId] = useState(-1);
+  const [loading, setLoading] = useState(false);
+  const triggerRef = useRef(null);
+
+  useEffect(() => {
+    loadPost(setLoading, lastPostId, data, setData);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [lastPostId]);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      (entries) => {
+        if (entries[0].isIntersecting && !loading) {
+          const lastId = [...data].pop()?.id ?? 1;
+          setLastPostId(lastId);
+        }
       },
-    ],
-  };
+      { threshold: 1.0 }
+    );
+    const ref = triggerRef.current;
+    if (ref) observer.observe(ref);
+
+    return () => {
+      if (ref) observer.unobserve(ref);
+    };
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [loading]);
 
   return (
     <>
       <div className="text-xl font-bold">피드</div>
-      {data.posts.map((item, index) => (
+      {data.map((item, index) => (
         <Post key={index} postData={item} page={'mypage'} />
       ))}
+      <div ref={triggerRef}>{loading && <h1>loading...</h1>}</div>
     </>
   );
 }
