2d538de77ff902990d41133466e34cb6e1ed7c44
esthel <esthel9026@naver.com>
2024-07-25 00:24:41 +0900

    fix: tailwind 우선순위 제어

--- stories/Button.tsx
+++ stories/Button.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { twMerge } from 'tailwind-merge';
 
 export interface ButtonProps {
   label: string;
@@ -14,17 +15,17 @@ export const Button = ({
   ...props
 }: ButtonProps) => {
   const baseStyle =
-    'py-1 px-2 inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50';
+    'py-1 px-2 inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors  focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50';
 
   const mode =
     variant === 'destructive'
       ? 'bg-destructive text-destructive-foreground hover:bg-destructive/90'
       : variant === 'outline'
-        ? 'border border-primary bg-accent text-primary hover:font-bold hover:p-6'
+        ? 'border border-primary bg-primary-foreground text-primary hover:font-bold'
         : variant === 'secondary'
-          ? 'bg-secondary text-secondary-foreground'
+          ? 'bg-secondary text-secondary-foreground hover:bg-secondary/80'
           : variant === 'ghost'
-            ? 'hover:bg-accent hover:text-accent-foreground'
+            ? 'text-black hover:bg-accent hover:text-accent-foreground'
             : variant === 'link'
               ? 'text-primary underline-offset-4 hover:underline'
               : 'bg-primary text-primary-foreground hover:bg-primary/90';
@@ -32,7 +33,7 @@ export const Button = ({
   return (
     <button
       type="button"
-      className={[baseStyle, mode, className].join(' ')}
+      className={twMerge([baseStyle, mode, className].join(' '))}
       {...props}
     >
       {label}
