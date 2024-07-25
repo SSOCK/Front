7a2048308601857f09399a6c159c88068eb665f5
esthel <esthel9026@naver.com>
2024-07-25 20:59:12 +0900

    fix: shadcn에서 사용하기 위해 button 수정

--- stories/Button.tsx
+++ stories/Button.tsx
@@ -1,42 +1,43 @@
 import React from 'react';
+import { Slot } from '@radix-ui/react-slot';
 import { twMerge } from 'tailwind-merge';
 
-export interface ButtonProps {
+export interface ButtonProps
+  extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   label: string;
   variant?: 'destructive' | 'outline' | 'secondary' | 'ghost' | 'link';
   className?: string;
-  onClick?: () => void;
+  asChild?: boolean;
 }
 
-export const Button = ({
-  label,
-  variant,
-  className,
-  ...props
-}: ButtonProps) => {
-  const baseStyle =
-    'py-1 px-2 inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors  focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50';
+export const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
+  ({ label, variant, className, asChild = false, ...props }, ref) => {
+    const baseStyle =
+      'py-1 px-2 inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors  focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50';
 
-  const mode =
-    variant === 'destructive'
-      ? 'bg-destructive text-destructive-foreground hover:bg-destructive/90'
-      : variant === 'outline'
-        ? 'border border-primary bg-primary-foreground text-primary hover:font-bold'
-        : variant === 'secondary'
-          ? 'bg-secondary text-secondary-foreground hover:bg-secondary/80'
-          : variant === 'ghost'
-            ? 'text-black hover:bg-accent hover:text-accent-foreground'
-            : variant === 'link'
-              ? 'text-primary underline-offset-4 hover:underline'
-              : 'bg-primary text-primary-foreground hover:bg-primary/90';
+    const mode =
+      variant === 'destructive'
+        ? 'bg-destructive text-destructive-foreground hover:bg-destructive/90'
+        : variant === 'outline'
+          ? 'border border-primary bg-primary-foreground text-primary hover:font-bold'
+          : variant === 'secondary'
+            ? 'bg-secondary text-secondary-foreground hover:bg-secondary/80'
+            : variant === 'ghost'
+              ? 'text-black hover:bg-accent hover:text-accent-foreground'
+              : variant === 'link'
+                ? 'text-primary underline-offset-4 hover:underline'
+                : 'bg-primary text-primary-foreground hover:bg-primary/90';
 
-  return (
-    <button
-      type="button"
-      className={twMerge([baseStyle, mode, className].join(' '))}
-      {...props}
-    >
-      {label}
-    </button>
-  );
-};
+    const Comp = asChild ? Slot : 'button';
+    return (
+      <Comp
+        className={twMerge([baseStyle, mode, className].join(' '))}
+        ref={ref}
+        {...props}
+      >
+        {label}
+      </Comp>
+    );
+  }
+);
+Button.displayName = 'Button';
