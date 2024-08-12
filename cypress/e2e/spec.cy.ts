76f780b6c9541097eea75e2169cebbda26c3c1ca
esthel <esthel9026@naver.com>
2024-08-12 22:59:12 +0900

    add: cypress test
    로그인 로직 검증

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -1,5 +1,17 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io');
+describe('Login', () => {
+  it('Wrong Uswer', () => {
+    cy.visit('http://localhost:3000/signin');
+    cy.get('#email').type('test@test.com');
+    cy.get('#password').type('123123');
+    cy.get('#signin').click();
+    cy.contains('아이디와 비밀번호를 다시 입력해주세요.');
+  });
+
+  it('Right Uswer', () => {
+    cy.visit('http://localhost:3000/signin');
+    cy.get('#email').type('test@test.com');
+    cy.get('#password').type('testtest');
+    cy.get('#signin').click();
+    cy.url().should('eq', 'http://localhost:3000/');
   });
 });
